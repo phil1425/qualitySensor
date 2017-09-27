a051f93f3e420043028213334812fa66c8017e07
Philipp Kollenz <philippkollenz@Philipps-MacBook-Pro.local>
2017-09-27 12:33:19 +0200

    first commit

---
+++ neuralnet.py
@@ -0,0 +1,29 @@
+import tflearn
+from tflearn import data_preprocessing
+from tflearn.layers.core import input_data, dropout, fully_connected, reshape
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.metrics import Accuracy
+
+acc = Accuracy()
+
+network = input_data(shape=[None, 640, 480, 3])
+
+network = conv_2d(network, 4, 5, strides=2, activation='relu', name = 'conv1')
+network = max_pool_2d(network, 2, strides=2)
+network = conv_2d(network, 4, 5, strides=1, activation='relu', name = 'conv2')
+network = conv_2d(network, 4, 3, strides=1, activation='relu', name = 'conv3')
+network = max_pool_2d(network, 2, strides=2)
+
+network = fully_connected(network, 128, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 128, activation='tanh')
+network = dropout(network, 0.5)
+
+network = fully_connected(network, 2, activation='softmax')
+
+network = regression(network, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001, metric=acc)
+
+model = tflearn.DNN(network, checkpoint_path='models/model-', best_checkpoint_path='models/best-model-')
---
+++ train.py
@@ -0,0 +1,33 @@
+import neuralnet as net
+import numpy as np
+import sys
+import h5py
+
+from tflearn.data_utils import image_preloader
+from tflearn.data_flow import ArrayFlow
+from tflearn import reshape
+
+epochs = int(sys.argv[1])
+
+X, Y = image_preloader('sleevesPhotos/training',
+                        image_shape=(480, 640),
+                        grayscale=False,
+                        mode='folder',
+                        categorical_labels=True,
+                        normalize=True)
+
+val_X, val_Y = image_preloader('sleevesPhotos/validation',
+                                image_shape=(480, 640),
+                                grayscale=False,
+                                mode='folder',
+                                categorical_labels=True,
+                                normalize=True)
+model = net.model
+
+model.fit(X,Y,
+            n_epoch=epochs,
+            validation_set=(val_X,val_Y),
+            show_metric=True,
+            run_id="deep_nn")
+
+model.save('models/final-model.tflearn')
